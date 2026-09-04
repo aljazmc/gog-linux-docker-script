0ff1c8bb265fa0100cbcdbf65ce3694243ecd0d5
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-04 10:46:57 +0200

    feat: delegate Docker image creation to 'dockerfiles' repository

--- project.sh
+++ project.sh
@@ -25,99 +25,39 @@ fi
 
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
-PROJECT_LANG=$LANG
 
 clean() {
 
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf GOG\ Games/ \
-    docker-compose.yml \
-    Dockerfile
+    docker compose down -v --rmi all --remove-orphans
+    rm -rf GOG\ Games/ \
+        docker-compose.yml
 
 }
 
 start() {
 
-  if [[ ! -f Dockerfile ]]; then
-    touch Dockerfile && \
-    cat <<EOF> Dockerfile
-  FROM debian:bookworm
-
-  ENV DEBIAN_FRONTEND=noninteractive
-  ENV USER=$USER
-
-  RUN apt-get update && \
-    apt-get install -y \
-    gtk2-engines \
-    gtk2-engines-pixbuf \
-    gtk2-engines-murrine \
-    libasound2-data \
-    libasound2 \
-    libasound2-plugins \
-    libc6 \
-    libcanberra-gtk-module \
-    libcurl4 \
-    libegl1-mesa \
-    libgconf-2-4 \
-    libgl1-mesa-dri \
-    libgl1-mesa-glx \
-    libglapi-mesa \
-    libgles2-mesa \
-    libgtk2.0-0 \
-    libnss3 \
-    libpng16-16 \
-    libpng-dev \
-    libxml2 \
-    libxt6 \
-    libxtst6 \
-    libudev-dev \
-    locales \
-    locales-all \
-    mesa-opencl-icd \
-    mesa-va-drivers \
-    mesa-vdpau-drivers \
-    sudo \
-    dosbox
-
-  ENV LC_ALL $PROJECT_LANG
-  ENV LANG $PROJECT_LANG
-  ENV LANGUAGE $PROJECT_LANG
-
-  RUN groupadd -g $PROJECT_GID -r $USER
-  RUN useradd -u $PROJECT_UID -g $PROJECT_GID --create-home -r $USER
-
-  #Change password
-  RUN echo "$USER:$USER" | chpasswd
-  #Make sudo passwordless
-  RUN echo "$USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-$USER
-  RUN usermod -aG sudo $USER
-  RUN usermod -aG plugdev $USER
-
-  USER $USER
-
-  WORKDIR /home/$USER
-EOF
-fi
-
 if [[ ! -f docker-compose.yml ]]; then
-  touch docker-compose.yml
-  cat <<EOF> docker-compose.yml
-  services:
+    touch docker-compose.yml
+    cat <<EOF> docker-compose.yml
+services:
     gogplay:
-      build: .
-      user: $PROJECT_UID:$PROJECT_GID
-      environment:
-        DISPLAY: $DISPLAY
-        XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
-      working_dir: "/home/$USER"
-      volumes:
-        - /tmp/.X11-unix:/tmp/.X11-unix
-        - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
-        - .:/home/$USER/source
-        - "./GOG\ Games:/home/$USER/GOG\ Games"
-      devices:
-        - /dev/snd:/dev/snd
-        - /dev/dri:/dev/dri
+        image: aljazmc/x11-debian
+        user: $(id -u):$(id -g)
+        working_dir: /home/x11
+        volumes:
+            - .:/home/x11
+            - /home/$USER/.Xauthority:/home/x11/.Xauthority
+            - /run/user/$(id -u):/run/user/1000
+            - /tmp/.X11-unix:/tmp/.X11-unix
+            - /var/lib/dbus/machine-id:/var/lib/dbus/machine-id
+        devices:
+            - /dev/dri:/dev/dri
+            - /dev/snd:/dev/snd
+        environment:
+            DISPLAY: $DISPLAY
+            HOME: /home/x11
+            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+        network_mode: host
 EOF
 fi
 
@@ -126,7 +66,7 @@ fi
   if [[ ! -d GOG\ Games ]]; then
     mkdir -p GOG\ Games 
     find ./* -maxdepth 0 -name "*.sh" -exec chmod +x {} +
-    find ./* -maxdepth 0 -name "*.sh" ! -name "project.sh" -exec docker compose run --rm gogplay sh -c 'cd /home/$USER/source && ./{}' \;
+    find ./* -maxdepth 0 -name "*.sh" ! -name "project.sh" -exec docker compose run --rm gogplay sh -c 'cd /home/x11 && ./{}' \;
   fi
 
 ## Find start.sh and run it
